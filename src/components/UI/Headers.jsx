50bc734a7bcd5a80b5e0828763e36ce256e33750
MasirJafri1 <masirjafri1@gmail.com>
2024-11-13 20:44:01 +0530

    Added mobile responsiveness to Navbar

--- src/components/UI/Headers.jsx
+++ src/components/UI/Headers.jsx
@@ -1,39 +1,48 @@
-import React from "react";
 import { NavLink } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
 
-function Headers() {
+export const Headers = () => {
+  const [show, setShow] = useState(false);
+
+  const handleButtonToggle = () => {
+    return setShow(!show);
+  };
+
   return (
-    <>
-      <header>
-        <div className="container">
-          <div className="grid navbar-grid">
-            <div className="Logo">
-              <NavLink to="/">
-                <h1>GL0B0VIA</h1>
-              </NavLink>
-            </div>
+    <header>
+      <div className="container">
+        <div className="grid navbar-grid">
+          <div className="Logo">
+            <NavLink to="/">
+              <h1>WorldAtlas</h1>
+            </NavLink>
+          </div>
 
-            <nav>
-              <ul>
-                <li>
-                  <NavLink to="/">Home</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/about">About</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/country">Country</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/contact">Contact</NavLink>
-                </li>
-              </ul>
-            </nav>
+          <nav className={show ? "menu-mobile" : "menu-web"}>
+            <ul>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/about">About</NavLink>
+              </li>
+              <li>
+                <NavLink to="/country">Country</NavLink>
+              </li>
+              <li>
+                <NavLink to="/contact">Contact</NavLink>
+              </li>
+            </ul>
+          </nav>
+
+          <div className="ham-menu">
+            <button onClick={handleButtonToggle}>
+              <GiHamburgerMenu />
+            </button>
           </div>
         </div>
-      </header>
-    </>
+      </div>
+    </header>
   );
-}
-
-export default Headers;
+};
